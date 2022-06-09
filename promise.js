bd99d0fa821fd68f359be1f0c920fd2ebb09db3e
gandatadyo <gandatadyosurya@gmail.com>
2022-06-09 21:26:39 +0700

    submit tugas

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise(async (resolve) => {
+    let ctEmosi = 0
+    const dataTheaterIXX = await promiseTheaterIXX();
+    for (const i in dataTheaterIXX) {
+      if (dataTheaterIXX[i].hasil == emosi) {
+        ctEmosi++
+      }
+    }
+
+    const dataTheaterVGC = await promiseTheaterVGC();
+    for (const i in dataTheaterVGC) {
+      if (dataTheaterIXX[i].hasil == emosi) {
+        ctEmosi++
+      }
+    }
+    resolve(ctEmosi)
+
+  })
+};
 
 module.exports = {
   promiseOutput,
